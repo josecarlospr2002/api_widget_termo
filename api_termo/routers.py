2a28d52edbcea5beecf1cc327e51504e59ba5cb8
josecarlospr2002 <josecarlosop23miami@gmail.com>
2026-03-11 07:55:27 -0700

    Modificación del routers.py

--- api_termo/routers.py
+++ api_termo/routers.py
@@ -1,18 +1,10 @@
 class BloquesRouter:
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'bloques':
-            return 'bbddt'   # siempre leer desde la base legacy
+            return 'bbddt'
         return None
-
-    def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'bloques':
-            return 'bbddt'   # aunque no escribas, lo dejamos claro
-        return None
-
-    def allow_relation(self, obj1, obj2, **hints):
-        return True
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == 'bloques':
-            return False  # no migrar estas tablas
+            return False
         return None
